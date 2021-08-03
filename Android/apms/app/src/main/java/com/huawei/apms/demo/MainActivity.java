0e9e1242cf06167338225999236eb29ded099a0b
MindaXia <xiaminyan1@huawei.com>
2021-08-03 16:09:51 +0800

    Update some detail

--- Android/apms/app/src/main/java/com/huawei/apms/demo/MainActivity.java
+++ Android/apms/app/src/main/java/com/huawei/apms/demo/MainActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020. Huawei Technologies Co., Ltd. All rights reserved.
+ * Copyright 2021. Huawei Technologies Co., Ltd. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,11 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
-package com.huawei.apms.demo;
+ package com.huawei.apms.demo;
 
 import android.os.Bundle;
-
 import com.huawei.agconnect.apms.APMS;
 import com.huawei.agconnect.apms.androiddemo.R;
 import com.huawei.agconnect.apms.custom.CustomTrace;
@@ -25,8 +23,9 @@ import com.huawei.agconnect.apms.instrument.AddCustomTrace;
 import com.huawei.util.HttpUtil;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -34,7 +33,8 @@ import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private boolean anrTestEnable = false;
+    private static final String TAG = "apmsAndroidDemo";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,17 +43,19 @@ public class MainActivity extends AppCompatActivity {
         // Initialize button event
         initNetworkEventButton();
         initApmsSwitchButton();
+        initApmsAnrSwitchButton();
         initCustomEventButton();
+        initAnrTestButton();
     }
 
     // define custom event by annotation
-    @AddCustomTrace(name = "自定义事件2")
+    @AddCustomTrace(name = "CustomEvent2")
     public void customEventHandle() {
     }
 
     // define custom event by code
     public void sendCustomEvent() {
-        CustomTrace customTrace = APMS.getInstance().createCustomTrace("自定义事件1");
+        CustomTrace customTrace = APMS.getInstance().createCustomTrace("CustomEvent1");
         customTrace.start();
         // code you want trace
         businessLogicStart(customTrace);
@@ -66,43 +68,69 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void businessLogicStart(CustomTrace customTrace) {
-        customTrace.putMeasure("处理次数", 0);
+        customTrace.putMeasure("ProcessingTimes", 0);
         for (int i = 0; i < 5; i++) {
-            customTrace.incrementMeasure("处理次数", 1);
+            customTrace.incrementMeasure("ProcessingTimes", 1);
         }
     }
 
     public void businessLogicEnd(CustomTrace customTrace) {
-        customTrace.putProperty("处理结果", "成功");
-        customTrace.putProperty("状态", "正常");
+        customTrace.putProperty("ProcessingResult", "Success");
+        customTrace.putProperty("Status", "Normal");
     }
 
+    public void initApmsAnrSwitchButton() {
+        findViewById(R.id.enable_apms_anr_off).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "disable apms.");
+                APMS.getInstance().enableAnrMonitor(false);
+            }
+        });
+        findViewById(R.id.enable_apms_anr_on).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "enable apms.");
+                APMS.getInstance().enableAnrMonitor(true);
+            }
+        });
+    }
 
     public void initApmsSwitchButton() {
         findViewById(R.id.enable_apms_off).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("apmsAndroidDemo", "disable apms.");
+                Log.d(TAG, "disable apms.");
                 APMS.getInstance().enableCollection(false);
             }
         });
         findViewById(R.id.enable_apms_on).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("apmsAndroidDemo", "enable apms.");
+                Log.d(TAG, "enable apms.");
                 APMS.getInstance().enableCollection(true);
             }
         });
     }
 
     public void initNetworkEventButton() {
-        Log.d("apmsAndroidDemo", "apms demo start.");
+        Log.d(TAG, "apms demo start.");
         Button sendNetworkRequestBtn = findViewById(R.id.btn_network);
         sendNetworkRequestBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("apmsAndroidDemo", "send network request.");
+                Log.d(TAG, "send network request.");
                 HttpUtil.oneRequest();
+            }
+        });
+    }
+
+    public void initAnrTestButton() {
+        findViewById(R.id.anr_test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "target anr");
+                anrTestEnable = true;
             }
         });
     }
@@ -111,21 +139,21 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.custom_normal_event).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("apmsAndroidDemo", "send a custom event");
+                Log.d(TAG, "send a custom event");
                 sendCustomEvent();
             }
         });
         findViewById(R.id.custom_normal_event_by_annotation).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("apmsAndroidDemo", "send a custom event by annotation");
+                Log.d(TAG, "send a custom event by annotation");
                 sendCustomEventByAnnotation();
             }
         });
         findViewById(R.id.custom_network_event).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("apmsAndroidDemo", "send a custom network event");
+                Log.d(TAG, "send a custom network event");
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
@@ -156,5 +184,28 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (anrTestEnable) {
+            try {
+                Thread.sleep(20000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return super.dispatchKeyEvent(event);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (anrTestEnable) {
+            try {
+                Thread.sleep(20000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
